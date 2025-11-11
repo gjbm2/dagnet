0c7ffe89dbb421ccc748eceb630298c409c53389
gjbm2 <github@gregmarsh.co.uk>
2025-11-11 21:26:24 +0000

    Vercel api fixes

---
+++ graph-editor/api/lib/__init__.py
@@ -0,0 +1,12 @@
+"""
+DagNet Graph Compute Library
+
+Python implementation of graph algorithms for DagNet.
+"""
+
+__version__ = "0.1.0"
+
+# Core types
+from . import graph_types
+
+__all__ = ['graph_types']
